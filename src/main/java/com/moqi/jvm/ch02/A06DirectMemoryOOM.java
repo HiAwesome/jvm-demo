ee245f4399a2474a8d5c7840b113b3443c398808
moqi <moqimoqidea@gmail.com>
2020-09-29 14:58:46 +0800

    使用unsafe分配本机内存

---
+++ src/main/java/com/moqi/jvm/ch02/A06DirectMemoryOOM.java
@@ -0,0 +1,35 @@
+package com.moqi.jvm.ch02;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * 使用unsafe分配本机内存
+ * VM Args：-Xmx20M -XX:MaxDirectMemorySize=10M
+ *
+ * Exception in thread "main" java.lang.OutOfMemoryError
+ *   at sun.misc.Unsafe.allocateMemory(Native Method)
+ *   at org.fenixsoft.oom.DMOOM.main(DMOOM.java:20)
+ *
+ * 复现书中异常失败，JDK 为自己编译的 JDK12
+ *
+ * @author moqi
+ * On 9/29/20 14:54
+ */
+public class A06DirectMemoryOOM {
+
+    private static final int _1MB = 1024 * 1024;
+
+    public static void main(String[] args) throws Exception {
+        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+        unsafeField.setAccessible(true);
+        Unsafe unsafe = (Unsafe) unsafeField.get(null);
+
+        while (true) {
+            unsafe.allocateMemory(_1MB);
+        }
+
+    }
+
+}
