b29af957fe827ea03eb530388230f57705475c1e
moqi <moqimoqidea@gmail.com>
2020-10-13 22:28:58 +0800

    在测试的 openjdk10 中都存放在 Java 堆中，在 JDK 7 之前静态变量是存放在永久代上的

--- src/main/java/com/moqi/jvm/ch04/A01JHSDB_TestCase.java
+++ src/main/java/com/moqi/jvm/ch04/A01JHSDB_TestCase.java
@@ -4,9 +4,24 @@ package com.moqi.jvm.ch04;
  * -Xmx10m -XX:+UseSerialGC -XX:-UseCompressedOops
  *
  * staticObj, instanceObj, localObj 存放在哪里？
+ * 在测试的 openjdk10 中都存放在 Java 堆中，在 JDK 7 之前静态变量是存放在永久代上的
  *
- * 测试 N 个 JDK 版本，没有实现书中效果
- * 参考 https://yuck1125.github.io/2019/10/17/use-HSDB-verify-Class-in-heap/ 进行了一些练习
+ *
+ * 测试 N 个 JDK 版本，没有实现书中效果，执行到 jhsbd hsbd --pid (pid) 时：
+ * 大部分 JDK 报错是权限问题，无法启动，显示
+ *   Unable to connect to process ID (pid):
+ *   sun.jvm.hotspot.debugger.DebuggerException: Can't attach to the process. Could be caused by an incorrect pid or lack of privileges.
+ *
+ * openjdk10 和 openjdk9 可用
+ *
+ * hsdb> scanoops 0x000000011fa00000 0x0000000120400000 com.moqi.jvm.ch04.A01JHSDB_TestCase$ObjectHolder
+ * 0x000000011faa2658 com/moqi/jvm/ch04/A01JHSDB_TestCase$ObjectHolder
+ * 0x000000011faa2680 com/moqi/jvm/ch04/A01JHSDB_TestCase$ObjectHolder
+ * 0x000000011faa2690 com/moqi/jvm/ch04/A01JHSDB_TestCase$ObjectHolder
+ *
+ * 其他参考，也进行了一些练习
+ * 1. https://www.iteye.com/blog/rednaxelafx-1847971
+ * 2. https://yuck1125.github.io/2019/10/17/use-HSDB-verify-Class-in-heap/
  *
  * @author moqi On 10/13/20 15:07
  */
