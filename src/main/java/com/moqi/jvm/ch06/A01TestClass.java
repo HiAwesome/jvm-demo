1e10c653c29628c131741ba6e1ceeb4142b47a8e
moqi <moqimoqidea@gmail.com>
2020-10-15 10:08:05 +0800

    测试 class 文件

---
+++ src/main/java/com/moqi/jvm/ch06/A01TestClass.java
@@ -0,0 +1,17 @@
+package com.moqi.jvm.ch06;
+
+/**
+ * 简单的 Java 代码
+ *
+ * @author moqi On 10/15/20 10:04
+ */
+
+public class A01TestClass {
+
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+
+}
