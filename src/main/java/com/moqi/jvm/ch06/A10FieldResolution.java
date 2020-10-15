ae7f4232df74b69b77e5c909e36ae0ad5040c277
moqi <moqimoqidea@gmail.com>
2020-10-15 17:06:01 +0800

    字段解析

---
+++ src/main/java/com/moqi/jvm/ch06/A10FieldResolution.java
@@ -0,0 +1,37 @@
+package com.moqi.jvm.ch06;
+
+/**
+ * 字段解析
+ *
+ * @author moqi On 10/15/20 17:03
+ */
+
+public class A10FieldResolution {
+
+    interface Interface0 {
+        int A = 0;
+    }
+
+    interface Interface1 extends Interface0 {
+        int A = 1;
+    }
+
+    interface Interface2 {
+        int A = 2;
+    }
+
+    static class Parent implements Interface1 {
+        public static int A = 3;
+    }
+
+    static class Sub extends Parent implements  Interface2 {
+        public static int A = 4;
+    }
+
+    public static void main(String[] args) {
+        // Sub.A = 4
+        System.out.println("Sub.A = " + Sub.A);
+    }
+
+
+}
