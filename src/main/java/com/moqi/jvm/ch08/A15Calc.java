630cabfc74f93b124916512c20eccdccc0e67b14
moqi <moqimoqidea@gmail.com>
2020-10-16 19:15:24 +0800

    一段简单的算术代码

---
+++ src/main/java/com/moqi/jvm/ch08/A15Calc.java
@@ -0,0 +1,18 @@
+package com.moqi.jvm.ch08;
+
+/**
+ * 一段简单的算术代码
+ *
+ * @author moqi On 10/16/20 17:53
+ */
+
+public class A15Calc {
+
+    public int calc() {
+        int a = 100;
+        int b = 200;
+        int c = 300;
+        return (a + b) * c;
+    }
+
+}
