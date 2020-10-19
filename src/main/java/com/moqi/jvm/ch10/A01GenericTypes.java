594412ebcdbe866b85c2465f1b4b3d6b8fd6b32a
moqi <moqimoqidea@gmail.com>
2020-10-19 16:50:08 +0800

    范型测试

---
+++ src/main/java/com/moqi/jvm/ch10/A01GenericTypes.java
@@ -0,0 +1,23 @@
+package com.moqi.jvm.ch10;
+
+/**
+ * 范型测试
+ *
+ * Oracle8u262 拒绝编译
+ *
+ * @author moqi On 10/19/20 16:48
+ */
+
+public class A01GenericTypes {
+
+    /*
+    private static String method(List<String> list) {
+        System.out.println("A01GenericTypes.method String");
+    }
+
+    private static int method(List<Integer> list) {
+        System.out.println("A01GenericTypes.method Integer");
+    }
+    */
+
+}
